5a0c54fe53226649fcdd34840bd639f134717207
Steve Maciaszek <stevechez@gmail.com>
2021-10-15 13:58:35 -0700

    many changes to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Header from './Header'
+import Feed from './components/Feed/Feed'
+import Login from './components/Login/Login';
+import Sidebar from './components/Sidebar/Sidebar';
+import Widgets from './components/Widgets/Widgets';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <h1>My Facebook Clone</h1>
-
-      <Header />
-      {/* app body */}
-        {/* sidebar
-        widgets */}
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase, { firestore } from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDRu-KWil7Wr2Kr8PP8OVEJpIXtJRHY5RM",
   authDomain: "facebook-clone-56597.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "704657167857",
   appId: "1:704657167857:web:a69b921bb8cd4e23bee050"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
