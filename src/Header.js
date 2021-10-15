177f197cd351be7670873f7153ec557fe0e9d191
stevechez <stevechez@gmail.com>
2021-10-15 07:44:02 -0700

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png" alt="facebook logo" />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRu-KWil7Wr2Kr8PP8OVEJpIXtJRHY5RM",
+  authDomain: "facebook-clone-56597.firebaseapp.com",
+  projectId: "facebook-clone-56597",
+  storageBucket: "facebook-clone-56597.appspot.com",
+  messagingSenderId: "704657167857",
+  appId: "1:704657167857:web:a69b921bb8cd4e23bee050"
+};
